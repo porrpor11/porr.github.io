ee5aca4e6bdd20e104ca3929526cc598c3cde572
porrpor11 <kissyme.por@gmail.com>
2018-11-01 03:10:01 +0700

    1

---
+++ wp-content/plugins/LayerSlider/static/js/greensock.js
@@ -0,0 +1,17 @@
+/*!
+* VERSION: 1.11.8
+* DATE: 2014-05-13
+* UPDATES AND DOCS AT: http://www.greensock.com
+*
+* @license Copyright (c) 2008-2014, GreenSock. All rights reserved.
+* This work is subject to the terms at http://www.greensock.com/terms_of_use.html or for
+* Club GreenSock members, the software agreement that was issued with your membership.
+*
+* @author: Jack Doyle, jack@greensock.com
+*/
+
+/*!
+* LayerSlider is using TweenLite, TimeLineLite, EasePack & CSSPlugin
+*/
+
+
